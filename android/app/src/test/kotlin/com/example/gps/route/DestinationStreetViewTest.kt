503d974c4efd551ff538dc002cba766ebd3dc1f7
xz64uj777 <xz64uj777@gmail.com>
2026-09-23 11:07:22 -0400

    Test keyless Street View arrival fallback

--- android/app/src/test/kotlin/com/example/gps/route/DestinationStreetViewTest.kt
+++ android/app/src/test/kotlin/com/example/gps/route/DestinationStreetViewTest.kt
@@ -13,4 +13,26 @@ class DestinationStreetViewTest {
     @Test fun linkRequestsPanoramaAtDestination() {
         assertEquals("https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=41.0000000%2C-72.0000000", DestinationStreetView.url(41.0, -72.0))
     }
+    @Test fun keylessModeWaitsUntilArrivalThenOpensExternalStreetView() {
+        assertEquals(
+            DestinationStreetView.DeliveryMode.WAIT_FOR_ARRIVAL,
+            DestinationStreetView.deliveryMode(enabled = true, hasStaticApiKey = false, arrived = false),
+        )
+        assertEquals(
+            DestinationStreetView.DeliveryMode.OPEN_EXTERNAL,
+            DestinationStreetView.deliveryMode(enabled = true, hasStaticApiKey = false, arrived = true),
+        )
+    }
+
+    @Test fun staticApiKeyKeepsInlinePhotoModeAndDisabledStaysOff() {
+        assertEquals(
+            DestinationStreetView.DeliveryMode.INLINE_PHOTO,
+            DestinationStreetView.deliveryMode(enabled = true, hasStaticApiKey = true, arrived = false),
+        )
+        assertEquals(
+            DestinationStreetView.DeliveryMode.DISABLED,
+            DestinationStreetView.deliveryMode(enabled = false, hasStaticApiKey = true, arrived = true),
+        )
+    }
+
 }
